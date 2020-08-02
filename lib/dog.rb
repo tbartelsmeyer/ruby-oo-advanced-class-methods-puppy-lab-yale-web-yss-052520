8592f29e036700fa3fa107769529ed4978c90169
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-08-02 13:39:45 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    puts @@all.map(&:name)
+  end
+
+end
